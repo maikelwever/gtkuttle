d9612ea0e87cc9f9931bbe9a596515fadfe572e3
Maikel Wever <maikelwever@gmail.com>
2013-12-09 15:59:17 +0100

    Command creation, edit option, custom options

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ gtkuttle
 ========
 
 A GTK sshuttle GUI with tray icon, written in Python.
+
+Requirements
+------------
+
+* PyGTK2
+* gksudo
+* sshuttle in $PATH
+
+
+Optional requirements
+---------------------
+
+* (x11-)ssh-askpass if you don't use keys for your ssh session
+
+
+Legal matter
+------------
+
+This software is licensed under the GNU GPL v3. See the LICENSE file for details. You can contribute back your changes to https://github.com/maikelwever/gtkuttle/.
--- gtkuttle.py
+++ gtkuttle.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python2
 #gtkuttle.py
+#
+# ===============================================================
+# gtkuttle Copyright (C) 2013 Maikel Wever.
+#
+# This program comes with ABSOLUTELY NO WARRANTY;
+# for details see the LICENSE file.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions; see the LICENCSE file for details.
+# ===============================================================
 
 import pygtk
 pygtk.require('2.0')
 import gtk
 import gtk.glade
 
-
 import json
 import os
+import time
+
+DEBUG = True
 
 try:
     import appindicator
@@ -28,6 +39,7 @@ class AddEndpointWindow():
         self.data.setdefault('name', '')
         self.data.setdefault('address', 'user@host:22')
         self.data.setdefault('subnet', '0.0.0.0/0')
+        self.data.setdefault('extra', '')
         self.data.setdefault('use_dns', False)
         self.data.setdefault('use_askpass', True)
 
@@ -40,6 +52,7 @@ class AddEndpointWindow():
         self.in_name = self.wTree.get_widget('in_name')
         self.in_address = self.wTree.get_widget('in_address')
         self.in_subnet = self.wTree.get_widget('in_subnet')
+        self.in_extra = self.wTree.get_widget('in_extra')
         self.cb_dns = self.wTree.get_widget('cb_dns')
         self.cb_askpass = self.wTree.get_widget('cb_askpass')
 
@@ -47,6 +60,7 @@ class AddEndpointWindow():
         self.in_name.set_text(self.data['name'])
         self.in_address.set_text(self.data['address'])
         self.in_subnet.set_text(self.data['subnet'])
+        self.in_extra.set_text(self.data['extra'])
         self.cb_dns.set_active(self.data['use_dns'])
         self.cb_askpass.set_active(self.data['use_askpass'])
 
@@ -57,6 +71,7 @@ class AddEndpointWindow():
         self.data['name'] = self.in_name.get_text()
         self.data['address'] = self.in_address.get_text()
         self.data['subnet'] = self.in_subnet.get_text()
+        self.data['extra'] = self.in_extra.get_text()
         self.data['use_dns'] = self.cb_dns.get_active()
         self.data['use_askpass'] = self.cb_askpass.get_active()
 
@@ -89,12 +104,30 @@ class MainApplication():
     def build_menu(self):
         self.menu = gtk.Menu()
 
+        if self.session_is_open():
+            disconnect_item = gtk.MenuItem("Disconnect current session")
+            disconnect_item.show()
+            self.menu.append(disconnect_item)
+            self.add_menu_seperator()
+
         # render saved endpoints
         for name, endpoint in self.settings['endpoints'].iteritems():
             endpoint_item = gtk.MenuItem(name)
             endpoint_item.show()
             self.menu.append(endpoint_item)
             endpoint_item.connect('activate', self.endpoint_clicked)
+
+        if len(self.settings['endpoints']):
+            self.add_menu_seperator()
+
+        for name, endpoint in self.settings['endpoints'].iteritems():
+            edit_ep_item = gtk.MenuItem("Edit {0}".format(name))
+            edit_ep_item.show()
+            self.menu.append(edit_ep_item)
+            edit_ep_item.connect('activate', self.edit_endpoint_clicked)
+
+        if len(self.settings['endpoints']):
+            self.add_menu_seperator()
 
         # render app buttons
         add_new = gtk.MenuItem("Add new endpoint")
@@ -112,6 +145,11 @@ class MainApplication():
         self.menu.show()
         self.ind.set_menu(self.menu)
 
+    def add_menu_seperator(self):
+        seperator = gtk.SeparatorMenuItem()
+        seperator.show()
+        self.menu.append(seperator)
+
     def read_settings(self):
         with open(SETTINGS_FILE_PATH, "r") as f:
             self.settings = json.loads(f.read())
@@ -125,15 +163,36 @@ class MainApplication():
 
     def disconnect_current_session(self):
         os.system("kill $(cat {0})".format(PID_FILE_PATH))
+        while self.session_is_open():
+            # Hmmmm....
+            # TODO: check if pidfile even gets removed...
+            time.sleep(1)
 
     def session_is_open(self):
         return os.path.exists(PID_FILE_PATH)
 
-    def show_add_new_dialog(self, widget):
-        dialog = AddEndpointWindow({})
+    def start_session(self, endpoint_data):
+        cmdline = "sshuttle -r {0} {1} {2} --daemon --pidfile {3}".format(
+            endpoint_data['address'],
+            endpoint_data['subnet'],
+            "--dns" if endpoint_data['use_dns'] else "",
+            PID_FILE_PATH
+        )
+        print cmdline
+        if DEBUG:
+            os.system(cmdline)
+        else:
+            os.system('gksudo "{0}"'.format(cmdline))
+
+    def show_add_new_dialog(self, widget, initial_data={}):
+        dialog = AddEndpointWindow(initial_data)
         result, data = dialog.run()
         print result
         print data
+
+        if not data['name']:
+            self.show_error("No name defined, try again")
+            return
 
         self.settings['endpoints'][data['name']] = data
         self.save_settings()
@@ -141,17 +200,39 @@ class MainApplication():
 
         print self.settings
 
+    def edit_endpoint_clicked(self, widget):
+        ep_name = widget.get_label()
+        try:
+            # TODO: fix ugly [5:] hax
+            endpoint = self.settings['endpoints'][ep_name[5:]]
+            del self.settings['endpoints'][ep_name[5:]]
+        except (IndexError, KeyError):
+            self.show_error("Endpoint not found in saved endpoint data. Something went wrong...")
+            return
+
+        self.show_add_new_dialog(widget, initial_data=endpoint)
+
     def endpoint_clicked(self, widget):
         ep_name = widget.get_label()
         try:
             endpoint = self.settings['endpoints'][ep_name]
         except (IndexError, KeyError):
+            self.show_error("Endpoint not found in saved endpoint data. Something went wrong...")
             return
 
         print endpoint
 
-        self.show_error("ERRUH!")
-        #import pdb; pdb.set_trace();
+        # Start sshuttle if no session is running.
+        if self.session_is_open():
+            resp = self.confirm_action("A session is already active. Cancel this session?")
+            print resp
+            if resp:
+                self.disconnect_current_session()
+            else:
+                return
+
+        # Start session here
+        self.start_session(endpoint)
 
     def run(self):
         gtk.main()
@@ -165,6 +246,17 @@ class MainApplication():
         )
         response = error_dialog.run()
         error_dialog.destroy()
+
+    def confirm_action(self, message):
+        confirm_dialog = gtk.MessageDialog(
+            message_format=message,
+            type=gtk.MESSAGE_QUESTION,
+            buttons=gtk.BUTTONS_YES_NO
+        )
+
+        response = confirm_dialog.run()
+        confirm_dialog.destroy()
+        return response
 
 
 if __name__ == "__main__":
